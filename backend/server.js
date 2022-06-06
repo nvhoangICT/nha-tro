30aa1ed632ac8cc913a56fc19c55787ab54fbd1f
Nguyen Viet Hoang <hoang.nv194769@sis.hust.edu.vn>
2022-06-07 03:13:03 +0700

    build server

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const configViewEngine = require('./src/config/viewEngine')
+const webRoutes = require('./src/routes/webRoutes')
+const dotenv = require('dotenv')
+
+let app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+configViewEngine(app)
+webRoutes(app)
+
+let port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+})
