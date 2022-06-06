2294fd624036ec7d94b166dd7688e9d981e7d381
Nguyen Viet Hoang <hoang.nv194769@sis.hust.edu.vn>
2022-06-07 02:46:12 +0700

    Change viewEngine config

--- backend/src/config/viewEngine.js
+++ backend/src/config/viewEngine.js
@@ -2,6 +2,8 @@ import express from "express"
 
 let configViewEngine = (app) => {
     app.use(express.static("./src/public")) // static files
+    app.set("view engine", "ejs")
+    app.set("views", "./src/views")
 }
 
 module.exports = configViewEngine;
