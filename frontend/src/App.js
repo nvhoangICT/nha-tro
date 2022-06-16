68ce6916d9392fa1854f1b587ea74b14214fe5fb
Nguyen Viet Hoang <hoang.nv194769@sis.hust.edu.vn>
2022-06-16 10:49:30 +0700

    Create GUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+// import "./public/css/open-iconic-bootstrap.min.css";
+// import "./public/css/animate.css";
+// import "./public/css/owl.carousel.min.css";
+// import "./public/css/owl.theme.default.min.css";
+// import "./public/css/magnific-popup.css";
+// import "./public/css/aos.css";
+// import "./public/css/ionicons.min.css";
+// import "./public/css/bootstrap-datepicker.css";
+// import "./public/css/jquery.timepicker.css";
+// import "./public/css/flaticon.css";
+// import "./public/css/icomoon.css";
+// import './public/css/style.css';
+
+import Home from './pages/Home';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../public/css/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
@@ -15,3 +20,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
