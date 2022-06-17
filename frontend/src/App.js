5df5ed3a2044d22249f33c0918b103f8bd226c8f
Nguyen Viet Hoang <hoang.nv194769@sis.hust.edu.vn>
2022-06-17 11:22:06 +0700

    Change GUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 }
