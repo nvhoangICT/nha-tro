b5887f6c159d214a7005d47337a211a016d16d0a
Nguyen Viet Hoang <hoang.nv194769@sis.hust.edu.vn>
2022-06-22 18:43:43 +0700

    Delete files

--- nha-tro-main/backend/src/config/viewEngine.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-let configViewEngine = (app) => {
-    app.use(express.static("./src/public")) // static files
-    app.set("view engine", "ejs")
-    app.set("views", "./src/views")
-}
-
-module.exports = configViewEngine;
