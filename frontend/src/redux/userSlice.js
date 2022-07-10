eff613d67c0bad7f20cc522379a0540743cbc449
Hoang Nguyen <hoang.nv194769@sis.hust.edu.vn>
2022-07-10 10:51:23 +0700

    redux-persist

---
+++ frontend/src/redux/userSlice.js
@@ -0,0 +1,31 @@
+const userSlice = createSlice ({
+    name: "user",
+    initialState: {
+        users: {
+            allUsers: null,
+            isFetching: false,
+            error: false
+        }
+    },
+    reducers: {
+        getUsersStart: (state) => {
+            state.login.isFetching = true;
+        },
+        getUsersSuccess: (state, action) => {
+            state.login.isFetching = false;
+            state.login.allUsers = action.payload;
+        },
+        getUsersFailed: (state) => {
+            state.login.isFetching = false;
+            state.login.error = true;
+        },
+    }
+});
+
+export const {
+    getUsersStart,
+    getUsersSuccess,
+    getUsersFailed
+} = userSlice.actions;
+
+export default userSlice.reducer;
