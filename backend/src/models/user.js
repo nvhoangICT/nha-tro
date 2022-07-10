2983e5c7247acd87ad88bdb83a19f15121b9a387
cuong <hiepsilontt@gmail.com>
2022-07-10 12:53:13 +0700

    forgot password

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -26,5 +26,41 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+
+  User.findByEmail = (email, result) => {
+    sql.query(`SELECT * from users WHERE email = '${email}'`, (err, res) => {
+        if (err) {
+            result(err, null);
+            return;
+        }
+        if (res.length) {
+            result(null, res[0])
+            return;
+        }
+        result(null, null);
+    });
+  }
+
+  User.resetPassword = (email, password, result) => {
+    sql.query(
+        "UPDATE users SET password = ? WHERE email = ?",
+        [password, email],
+        (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(null, err);
+                return;
+            }
+            if (res.affectedRows == 0) {
+                result({ kind: "not_found" }, null);
+                return;
+            }
+            result(null, { email: email });
+        }
+    );
+  };
+
   return User;
+
+  
 };
